f7035a8da7657875b5655299fe99feeefe7a56b2
vSketch <74068469+vSketch@users.noreply.github.com>
2023-06-01 20:07:47 -0400

    Fixed issues - Headers & Footers -Links -Photos -Added Map -Removed Gallery (Hidden) -Added to packages -Fixed form (hopefully) -Fixed gramatical errors -Fixed Dates
    June 1, 2023

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".wrapper i");
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+let ScrollToTop = document.querySelector("#ScrollToTop");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    ScrollToTop.style.display = "block";
+  } else {
+    ScrollToTop.style.display = "none";
+  }
 }
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-    });
-});
-
-const ScrollToTop = document.querySelector("#ScrollToTop");
-
-ScrollToTop.addEventListener("click", function () {
-    console.log("SET");
-    $("html, body").animate({ scrollTop: 0}, "slow")
-})
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 const menu = document.querySelector("body > div.nav-wrapper > div > div.menu")
 const burger = document.querySelector("body > div.nav-wrapper > div > div.logo-hamburger > div.hamburger")
